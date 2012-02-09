229463266d21c5335783a2690959c1d58ebad7a7
John Murphy <murphybytes@gmail.com>
2012-02-09 07:41:16 -0600

    mostly building out project structure

---
+++ lib/colony/message_channel.rb
@@ -0,0 +1,8 @@
+require 'colony/context'
+
+module Colony
+
+class MessageChannel 
+end
+
+end
---
+++ spec/message_channel_spec.rb
@@ -0,0 +1,8 @@
+$: << File.join( Dir.pwd, 'lib' )
+require 'colony'
+
+describe Colony::MessageChannel do
+  
+  it 'should retrieve list of nodes with a particular role' do
+  end
+end
